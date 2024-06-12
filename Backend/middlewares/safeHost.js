c8b7cfe6dbd8348422e0a36b6e686b640d03e651
Krishiv <steem.krishiv@gmail.com>
2024-06-12 12:45:18 +0545

    init2

--- Backend/middlewares/safeHost.js
+++ Backend/middlewares/safeHost.js
@@ -1,16 +1,15 @@
 /** verify requests host */
 const safeHost = (req, res, next) => {
-    const origin = req.headers.origin
-    console.log('origin', origin);
-    if (
-      origin === 'https://worldofxpilar.com' ||
-      origin === 'http://localhost' // development host
-    ) {
-      next()
-    } else {
-      res.status(403)
-    }
+  const origin = req.headers.origin
+  console.log('origin', origin);
+  if (
+    origin === 'https://steemauto.com' ||
+    origin === 'http://localhost' // development host
+  ) {
+    next()
+  } else {
+    res.status(403)
   }
-  
-  module.exports = safeHost
-  
+}
+
+module.exports = safeHost
